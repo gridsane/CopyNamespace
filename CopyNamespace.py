a03508752f122999dbd6e8bb0a074f5960f9611e
grid <gridsane@gmail.com>
2013-08-06 11:56:57 +0600

    fix rules to find class, interface and trait definitions, codestyle fixes

--- CopyNamespace.py
+++ CopyNamespace.py
@@ -1,47 +1,52 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
+
 
 class CopyPhpSniffer:
-	def __init__(self, view):
-		self.view = view
+    def __init__(self, view):
+        self.view = view
 
-	def find(self, expression):
-		expression = '[^|\n](?<='+expression+'\s)([a-z]|\\\\)*'
-		region = self.view.find(expression, 0, sublime.IGNORECASE)
-		if region != None and region.empty() == False:
-			result = self.view.substr(region)
-			if result != False:
-				return result.strip()
+    def find(self, expression):
+        expression = '(?<=' + expression + '\s)([a-z]|\\\\)*'
+        region = self.view.find(expression, 0, sublime.IGNORECASE)
+        print(region, expression)
+        if region is not None and region.empty() is False:
+            result = self.view.substr(region)
+            if result is not False:
+                return result.strip()
 
-		return False
+        return False
 
 
 class CopyNamespaceCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		sublime.set_clipboard('')
-		sniffer = CopyPhpSniffer(self.window.active_view())
-		namespace = sniffer.find('namespace');
-		if namespace != False:
-			sublime.set_clipboard(namespace);
-			sublime.status_message('Copied namespace: '+namespace)
-		else:
-			sublime.error_message('Could not find a namespace')
+    def run(self):
+        sublime.set_clipboard('')
+        sniffer = CopyPhpSniffer(self.window.active_view())
+        namespace = sniffer.find('namespace')
+        if namespace is not False:
+            sublime.set_clipboard(namespace)
+            sublime.status_message('Copied namespace: ' + namespace)
+        else:
+            sublime.error_message('Could not find a namespace')
+
 
 class CopyClassnameCommand(sublime_plugin.WindowCommand):
-	def run(self, with_namespace=False):
-		sublime.set_clipboard('')
-		sniffer = CopyPhpSniffer(self.window.active_view())
-		prefix = ''
-		if with_namespace == True:
-			namespace = sniffer.find('namespace')
-			if namespace != False:
-				prefix = namespace+'\\'
+    def run(self, with_namespace=False):
+        sublime.set_clipboard('')
+        sniffer = CopyPhpSniffer(self.window.active_view())
+        prefix = ''
+        if with_namespace is True:
+            namespace = sniffer.find('namespace')
+            if namespace is not False:
+                prefix = namespace + '\\'
 
-		for option in ['class', 'interface', 'trait']:
-			result = sniffer.find(option)
-			if result != False:
-				sublime.set_clipboard(prefix+result)
-				sublime.status_message('Copied '+option+': '+prefix+result)
-				break
+        for option in ['^abstract\sclass', '^class', '^final\sclass' '^interface', '^trait']:
+            result = sniffer.find(option)
+            print(result)
+            if result is not False:
+                sublime.set_clipboard(prefix + result)
+                sublime.status_message('Copied ' + option + ': ' + prefix + result)
+                break
 
-		if result == False:
-			sublime.error_message('Could not find a class, interface or trait.')
+        if result is False:
+            sublime.error_message('Could not find a class, interface or trait.')
